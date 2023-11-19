f824c87c3222b27d09b96813cd05d5d2260198df
timtonix <timtonic@outlook.fr>
2023-11-19 17:52:11 +0100

    init route

--- app.py
+++ app.py
@@ -1,2 +1,22 @@
 from flask import Flask, request, render_template
+import json
 
+
+app = Flask(__name__)
+key = json.load(open("key.json"))
+app.secret_key = key["key"]
+
+
+@app.route("/")
+def index():
+    return render_template("home.html")
+
+
+@app.route("/inscription", methods=("POST", "GET"))
+def inscription():
+    return render_template("inscription.html")
+
+
+
+if __name__ == "__main__":
+    app.run(port=5555)
